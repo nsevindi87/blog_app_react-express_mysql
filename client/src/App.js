bc2a99acba97a193e82962f5e76f1b840b7da08d
Nizami Sevindi <nsevindi87@gmail.com>
2023-04-06 09:51:27 +0200

    router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,64 @@
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import Write from './Pages/Write';
+import Home from './Pages/Home';
+import Single from './Pages/Single';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Outlet,
 } from "react-router-dom";
+import { Children } from 'react';
 
+const Layout = () =>{
+  return(
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/single",
+        element: <Single/>
+      },
+      {
+        path: "/write",
+        element: <Write/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element:<Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
 
 
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <RouterProvider router={router} />
     </div>
   );
 }
