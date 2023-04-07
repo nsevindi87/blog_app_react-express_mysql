bbcbd4f911860b8a1bab128490159f05da8342d0
Nizami Sevindi <nsevindi87@gmail.com>
2023-04-07 11:54:21 +0200

    server-route and controllers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import authRoutes from "./routes/auth.js"
+import usersRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 
 const app = express();
 
 app.use(express.json())
-
-app.get("/test", (req, res) => {
-    res.json("It works")
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8000, ()=>{
     console.log("Connected!!!!")
