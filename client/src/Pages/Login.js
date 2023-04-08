138944c630667b162a4c2bbb08ba05663af937b9
Nizami Sevindi <nsevindi87@gmail.com>
2023-04-08 09:55:52 +0200

    client login

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,14 +1,36 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [inputs,setInputs] = useState({
+    username:"",
+    password:""
+  })
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setInputs((prev) =>({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await axios.post("http://localhost:8000/api/auth/login", inputs)
+      navigate("/")
+    } catch (error) {
+      setError(error.response.data)
+    }
+  }
+
   return (
     <div className='auth'>
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder='username' />
-        <input required type="password" placeholder='password' />
-        <button>Login</button>
-        <p>This is an Error!</p>
+        <input required type="text" placeholder='username' name="username" onChange={handleChange}/>
+        <input required type="password" placeholder='password' name="password" onChange={handleChange}/>
+        <button onClick={handleSubmit}>Login</button>
+        {error && <p>{error}</p>}
         <span>Don't you have an account? <Link to="/register">Register</Link></span>
       </form>
     </div>
