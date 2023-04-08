07db7879df4fb92daec8167f740e6dfce5fbe591
Nizami Sevindi <nsevindi87@gmail.com>
2023-04-08 11:06:05 +0200

    context with logot

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import {db} from "../db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+//================= REGISTER
 export const register = (req,res)=>{
     //Check existing user
     const q = "SELECT * FROM users WHERE email = ? OR username = ?";
@@ -24,31 +25,35 @@ export const register = (req,res)=>{
     })
 }
 
-
-
-
+//================= LOGIN
 export const login = (req,res)=>{
 //Check user
 const q = "SELECT * FROM users WHERE username = ?"
 db.query(q, [req.body.username], (err,data)=>{
-    if(err) return res.json(err);
+    if(err) return res.status(500).json(err);
     if(data.lenght === 0) return res.status(404).json("User not found")
 
     //Check password
     const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password)
-    if(!isPasswordCorrect) return res.status(404).json("Wrong username or password") 
+
+    if(!isPasswordCorrect) return res.status(400).json("Wrong username or password") 
 
     const token = jwt.sign({id:data[0].id}, "jwtkey");
-    const {password, ...other} = data[0]
-
-    res.cookie("access_token", token,{
+    const {password, ...other} = data[0];
+//!Must be controlled
+   res.cookie("access_token", token,{
         httpOnly:true
     }).status(200).json(other)
-})
+}) 
 
 }
 
-
+//================= LOGOUT
 export const logout = (req,res)=>{
+
+    res.clearCookie("access_token",{
+        sameSite:"none",
+        secure:true
+    }).status(200).json("User has been logged out")
 
 }
